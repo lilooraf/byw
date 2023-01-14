6a1c82e716290cc5030713e18b3d39ef61d9db97
Raphael Hoareau <lilooraf35@gmail.com>
2023-01-14 03:27:15 +0100

    landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,46 @@
-import React from "react";
-import Layout from "../components/Layout";
+import React from 'react';
+import Layout from '../components/Layout';
+import Image from 'next/image';
 
 const Home = (props) => {
   return (
-    <Layout>
-    </Layout>
+    <div className=''>
+      <div className=''>
+        <div className='z-10 whitespace-nowrap absolute flex justify-center md:w-1/2 w-full md:mt-32 mt-3 text-transparent bg-clip-text bg-gradient-to-br from-white to-orange-600'>
+          <div>
+            <div className='text-5xl font-bold'>Welcome to</div>
+            <div className='text-5xl font-bold'>Bet You Win</div>
+            <div className='text-2xl font-bold'>The best way to win</div>
+          </div>
+        </div>
+      </div>
+      <div className='w-full flex justify-center'>
+        <div className='md:px-20 mt-40 md:mt-0 max-w-[70rem] md:max-w-80'>
+          <Image
+            src='/assets/wallpapers/background.png'
+            width={1920}
+            height={1280}
+          />
+        </div>
+      </div>
+      <div className=''>
+        <div className='mt-8 md:mt-32 pb-10'>
+          <div className='text-center px-10'>
+            <p className='text-3xl font-bold inline'>What is&nbsp;</p>
+            <p className='text-3xl font-bold inline text-transparent bg-clip-text bg-gradient-to-tl from-white to-orange-600'>
+              Bet You Win
+            </p>
+            <p className='text-3xl font-bold inline'>?</p>
+            <br />
+            <br />
+            <div className='text-2xl font-bold'>
+              It's a platform that allows you to bet on your favorite teams and
+              players.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
