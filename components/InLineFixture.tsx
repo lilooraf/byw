e227778d3fb7c460472b079a989205483d4daf0c
Raphael Hoareau <lilooraf35@gmail.com>
2023-01-28 04:18:01 +0100

    check-box

--- components/InLineFixture.tsx
+++ components/InLineFixture.tsx
@@ -10,11 +10,11 @@ const InLineFixture: React.FC<{ fixture: FixtureProps }> = ({ fixture }) => {
       <td className='w-4 p-4 hidden lg:table-cell'>
         <div className='flex items-center'>
           <input
-            id='checkbox-table-search-1'
+            id='checkbox-fixture'
             type='checkbox'
             className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
           />
-          <label htmlFor='checkbox-table-search-1' className='sr-only'>
+          <label htmlFor='checkbox-fixture' className='sr-only'>
             checkbox
           </label>
         </div>
--- pages/fixture/list.tsx
+++ pages/fixture/list.tsx
@@ -269,6 +269,14 @@ const Fixtures: React.FC<Props> = (props) => {
     );
   }
 
+  const changeAllCheckboxes = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const checkboxes: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[id=checkbox-fixture]');
+    
+    checkboxes.forEach((checkbox: HTMLInputElement) => {
+      checkbox.checked = e.target.checked;
+    });
+  };
+
   return (
     <div className='page mx-4 flex justify-center'>
       <main className='w-full'>
@@ -286,6 +294,9 @@ const Fixtures: React.FC<Props> = (props) => {
                   <input
                     id='checkbox-table-search-1'
                     type='checkbox'
+                    onChange={(e) => {
+                      changeAllCheckboxes(e);
+                    }}
                     className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
                   />
                   <label htmlFor='checkbox-table-search-1' className='sr-only'>
