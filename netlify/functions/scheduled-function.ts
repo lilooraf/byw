8efd9225f75bf3d62207f74ce46c1666c691c914
Raphael Hoareau <lilooraf35@gmail.com>
2023-01-28 03:13:06 +0100

    fix folder path

--- netlify/functions/scheduled-function.ts
+++ netlify/functions/scheduled-function.ts
@@ -1,6 +1,6 @@
 import { Handler, HandlerEvent, HandlerContext, schedule } from "@netlify/functions";
-import { seedFixtures, seedLeagues, seedStandigns } from "../../../api/logic";
-import { bywAlgo } from "../../../api/process";
+import { seedFixtures, seedLeagues, seedStandigns } from "../../api/logic";
+import { bywAlgo } from "../../api/process";
 
 const fetchApi: Handler = async (event: HandlerEvent, context: HandlerContext) => {
     console.log("Received event:", event);
