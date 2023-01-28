e933ec3b0b7d196fafb0980924b492f4db360927
Raphael Hoareau <lilooraf35@gmail.com>
2023-01-28 03:10:07 +0100

    scheduler

---
+++ prisma/netlify/functions/scheduled-function.ts
@@ -0,0 +1,25 @@
+import { Handler, HandlerEvent, HandlerContext, schedule } from "@netlify/functions";
+import { seedFixtures, seedLeagues, seedStandigns } from "../../../api/logic";
+import { bywAlgo } from "../../../api/process";
+
+const fetchApi: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+    console.log("Received event:", event);
+    
+    console.log(`Start fetching ...`);
+    await seedLeagues(5);
+    console.log(`Leagues Seeded`);
+    await seedFixtures();
+    console.log(`Fixtures Seeded.`);
+    await seedStandigns();
+    console.log(`Standings Seeded.`);
+    bywAlgo(100);
+    console.log(`Stop fetching`);
+
+    return {
+        statusCode: 200,
+    };
+};
+
+const handler = schedule("@daily", fetchApi)
+
+export { handler };
